55394cc95e1d2135fe21b7a5e544d026fb1c7589
adtidiane <diallotiji@gmail.com>
2015-05-27 13:08:32 +0100

    add app secretBallot

---
+++ secretballot/middleware.py
@@ -0,0 +1,20 @@
+from hashlib import md5
+
+
+class SecretBallotMiddleware(object):
+    def process_request(self, request):
+        request.secretballot_token = self.generate_token(request)
+
+    def generate_token(self, request):
+        raise NotImplementedError
+
+
+class SecretBallotIpMiddleware(SecretBallotMiddleware):
+    def generate_token(self, request):
+        return request.META['REMOTE_ADDR']
+
+
+class SecretBallotIpUseragentMiddleware(SecretBallotMiddleware):
+    def generate_token(self, request):
+        s = ''.join((request.META['REMOTE_ADDR'], request.META.get('HTTP_USER_AGENT', '')))
+        return md5(s).hexdigest()
